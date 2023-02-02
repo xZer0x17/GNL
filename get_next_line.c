f439eec5c6c4d495bce7851ee1ad42889d2ec65c
xZer0x17 <111791370+xZer0x17@users.noreply.github.com>
2023-02-02 20:40:36 +0100

    Mejor de leaks

--- get_next_line.c
+++ get_next_line.c
@@ -6,41 +6,48 @@
 /*   By: alflores <alflores@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/01/29 18:44:54 by alflores          #+#    #+#             */
-/*   Updated: 2023/02/02 19:18:45 by alflores         ###   ########.fr       */
+/*   Updated: 2023/02/02 20:13:23 by alflores         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "get_next_line.h"
 
-char	*get_bbc(char *bbc, int fd)
+char	*get_buffer(char *buffer, int fd)
 {
-	char	*buffer;
-	int		i;
 	char	*aux;
+	char	*aux_buffer;
+	int		i;
 	
-	if(!bbc)
-		bbc = calloc(sizeof(char), 1);
-	buffer = (char *)malloc(sizeof(char) * BUFFER_SIZE + 1);
 	if(!buffer)
+		buffer = ft_calloc(sizeof(char), 1);
+	aux	 = (char *)ft_calloc(sizeof(char), BUFFER_SIZE + 1);
+	if(!aux)
 		return (NULL);
 	i = 1;
-	while (i != 0)
+	while (!ft_strchr(aux, '\n') && i != 0)
 	{
-		aux = bbc;
-		i = read(fd, buffer, BUFFER_SIZE);
-		free (bbc);
-		bbc = ft_strjoin(aux, buffer);
+		aux_buffer = buffer;
+		i = read(fd, aux, BUFFER_SIZE);
+		if (i == -1)
+		{
+			free(aux);
+			free(buffer);
+			return (NULL);
+		}
+		free (buffer);
+		aux[i] = '\0';
+		buffer = ft_strjoin(aux_buffer, aux);
 	}
-	return (bbc);
+	free (aux);
+	return (buffer);
 }
-
 char	*get_line(char *buffer)
 {
 	char	*line;
 	int		i;
 
 	i = 0;
-	if (!buffer)
+	if (!buffer[i])
 		return (NULL);
 	while (buffer[i] != '\n' && buffer[i] != '\0')
 		i++;
@@ -48,63 +55,52 @@ char	*get_line(char *buffer)
 	if (!line)
 		return (NULL);
 	i = 0;
-	while (buffer[i] != '\0' && buffer[i] != '\n')
+	while (buffer[i] != '\n' && buffer[i] != '\0')
 	{
 		line[i] = buffer[i];
 		i++;
 	}
-	if (buffer[i] == '\n')
+	if (buffer[i] == '\n' && buffer[i])
 		line[i] = buffer[i];
 	return (line);
 }
 
-char	*get_chupadita_bbc(char *bbc)
+char	*get_clean_buffer(char *buffer)
 {
 	char	*trash;
 	int		l_line;
 	int		l_trash;
 	
 	l_line = 0;
-	while (bbc[l_line] != '\n' && bbc[l_line] != '\0')
+	while (buffer[l_line] != '\n' && buffer[l_line] != '\0')
 		l_line++;
+	if (!buffer[l_line])
+	{
+		free (buffer);
+		return (NULL);
+	}
 	l_line++;
-	trash = ft_calloc(sizeof(char), (ft_strlen(bbc) - l_line));
+	trash = malloc(sizeof(char) * (ft_strlen(buffer) - l_line));
 	if (!trash)
 		return (NULL);
 	l_trash = 0;
-	while (bbc[l_line] != '\0')
-	{
-		trash[l_trash++] = bbc[l_line++];
-	}
+	while (buffer[l_line])
+		trash[l_trash++] = buffer[l_line++];
+	free (buffer);
 	return (trash); 
 }
 
 char	*get_next_line(int fd)
 {
-	static char	*bbc;
+	static char	*buffer;
 	char		*line;
 	
 	if (fd < 0 || BUFFER_SIZE <= 0)
 		return (NULL);
-	bbc = get_bbc(bbc, fd);
-	if (!bbc)
+	buffer = get_buffer(buffer, fd);
+	if (!buffer)
 		return (NULL);
-	line = get_line(bbc);
-	bbc = get_chupadita_bbc(bbc);
+	line = get_line(buffer);
+	buffer = get_clean_buffer(buffer);
 	return (line);
 }
-
-/* int main()
-{
-	char	*aux;
-	int		fd= open("prueba1.txt", O_RDONLY);
-	
-	for(int i = 1; i <= 5; i++)
-	{
-		printf("1\n");
-		aux = get_next_line(fd);
-		printf("%s\n",aux);
-		free(aux);
-	}
-}
- */
